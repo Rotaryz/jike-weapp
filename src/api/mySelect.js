5dc32cd72fdf8d564fb8a7712c45bb13463328a8
philly <346692921@qq.com>
2017-12-18 15:48:32 +0800

    内容收藏

--- src/api/mySelect.js
+++ src/api/mySelect.js
@@ -19,7 +19,34 @@ export default class mySelect extends base {
    * @returns {Promise.<*>}
    */
   static async getContentList(page) {
-    const url = `${this.baseUrl}/api/contents/merchant-contents`
-    return await this.get(url)
+    const url = `${this.baseUrl}/api/info/per_content`
+    let data = {
+      page
+    }
+    return await this.get(url, data)
+  }
+  /**
+   * 收藏内容
+   * @param id 内容id
+   * @returns {Promise.<*>}
+   */
+  static async selectContent(id) {
+    const url = `${this.baseUrl}/api/info/store_content`
+    let data = {
+      content_id: id
+    }
+    return await this.get(url, data)
+  }
+  /**
+   * 取消内容收藏
+   * @param id 内容id
+   * @returns {Promise.<*>}
+   */
+  static async cancleSelectContent(id) {
+    const url = `${this.baseUrl}/api/info/destroy_content`
+    let data = {
+      content_id: id
+    }
+    return await this.get(url, data)
   }
 }
