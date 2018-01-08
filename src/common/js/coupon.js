9797df6546ad2c9cfac1a84a3e81f0a970d274ac
philly <346692921@qq.com>
2018-01-08 11:46:26 +0800

    merge

--- src/common/js/coupon.js
+++ src/common/js/coupon.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 export default class CouponFactory {
-  constructor({id, title, platform_price, shop_price, stock, promotion_type, end_at, not_allow_time, collected}) {
+  constructor({id, title, platform_price, shop_price, stock, promotion_type, end_at, not_allow_time, collected, shop_logo, shop_name}) {
     this.id = id
     this.title = title
     this.platform_price = Math.floor(platform_price)
@@ -10,5 +10,7 @@ export default class CouponFactory {
     this.promotion_type = promotion_type
     this.end_at = end_at ? end_at.split(' ')[0] : ''
     this.not_allow_time = not_allow_time
+    this.shop_logo = shop_logo
+    this.shop_name = shop_name
   }
 }
