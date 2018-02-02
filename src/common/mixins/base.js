8fd7130c46037ffea375a4c93bb70e81083e9005
ben <xuedengheng@qq.com>
2018-02-02 18:10:45 +0800

    fix some bug

--- src/common/mixins/base.js
+++ src/common/mixins/base.js
@@ -2,6 +2,8 @@ import wepy from 'wepy'
 import Tips from 'common/js/tips'
 import Info from 'api/info'
 
+const COLLECT_COUNT = 20
+
 export default class base extends wepy.mixin {
   loaded() {
     this.init = true
@@ -19,7 +21,7 @@ export default class base extends wepy.mixin {
     const now = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
     const collectDate = wepy.getStorageSync('collectDate')
     const collectLength = wepy.getStorageSync('collectLength')
-    if (now === collectDate && collectLength < 7) {
+    if (now === collectDate && collectLength < COLLECT_COUNT) {
       await Info.collectFormIds({form_ids: [formId]})
       wepy.setStorageSync('collectLength', collectLength + 1)
     } else if (now !== collectDate) {
