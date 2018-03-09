d65ecd58ada964faf4eb8c1739844c0cd53aba4a
philly <346692921@qq.com>
2018-03-09 12:15:00 +0800

    推荐页面处理显示问题

--- src/api/merchants.js
+++ src/api/merchants.js
@@ -21,6 +21,36 @@ export default class Merchants extends base {
   }
 
   /**
+   * 获取推荐商家详情
+   * @param id  商家Id
+   * @returns {Promise.<*>}
+   */
+  static async getMerchantsStatus(id, data, loading) {
+    const url = `${this.baseUrl}/api/merchants/show-merchant/${id}`
+    return await this.get(url, data, loading)
+  }
+
+  /**
+   * 获取推荐商家礼包(全部)
+   * @param id  商家Id
+   * @returns {Promise.<*>}
+   */
+  static async getGiftBags(id, data, loading) {
+    const url = `${this.baseUrl}/api/merchants/show-gift-bag/${id}`
+    return await this.get(url, data, loading)
+  }
+
+  /**
+   * 获取推荐商家优惠券列表(分页)
+   * @param id  商家Id
+   * @returns {Promise.<*>}
+   */
+  static async getCoupons(id, data, loading) {
+    const url = `${this.baseUrl}/api/merchants/show-promotion/${id}`
+    return await this.get(url, data, loading)
+  }
+
+  /**
    * 保存浏览记录
    * @param data
    * @returns {Promise.<*>}
