4d29698cc8d7f841aad60d01d73e93bbcdcaa44e
Rotary <849307221@qq.com>
2018-04-23 16:42:57 +0800

    对接分享详情页面

--- src/api/sharemoney.js
+++ src/api/sharemoney.js
@@ -4,8 +4,32 @@
 import base from './base'
 
 export default class shareMoney extends base {
+  /**
+   * 分享活动列表
+   * @param data
+   * @returns {Promise.<*>}
+   */
   static async shareList (data) {
     const url = `${this.baseUrl}/api/sharemoney/act-list`
     return await this.get(url, data)
+  }
+
+  /**
+   * 分享活动详情
+   * @param data
+   * @returns {Promise.<*>}
+   */
+  static async shareDetail (id) {
+    const url = `${this.baseUrl}/api/sharemoney/act-promotion-info/${id}`
+    return await this.get(url)
+  }
+  /**
+   * 佣金公告提示
+   * @param data
+   * @returns {Promise.<*>}
+   */
+  static async profitNotice () {
+    const url = `${this.baseUrl}/api/sharemoney/profit-notice`
+    return await this.get(url)
   }
 }
